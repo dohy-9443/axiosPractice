affeb455be6ffecf3d57c291908b82de3fa04658
dohy-9443 <79897962+dohy-9443@users.noreply.github.com>
2022-07-02 16:32:02 +0900

    Feat: react-query 추가 , 공식 문서 예제 직접 작성
    유튜브 우아한 tech -> React query와 상태관리 영상 보며 react-query 맛보고 이해해보기

--- axios_practice/src/Pages/Main/MainPage.js
+++ axios_practice/src/Pages/Main/MainPage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import axios from "axios";
 
 import * as S from "./style";
@@ -20,8 +21,16 @@ export const ListMap = ({ list, component }) => {
   );
 };
 
+const queryClient = new QueryClient()
+
 const MainPage = () => {
   const [users, setUsers] = useState([]);
+
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    getUsersApi();
+  }, []);
 
   const getUsersApi = async () => {
     const ip = "https://jsonplaceholder.typicode.com";
@@ -30,24 +39,54 @@ const MainPage = () => {
       // const response = await axios.get(`${ip}/users`);
       fetch(`${ip}/users`)
         .then((res) => res.json())
-        .then((data) => console.log(data));
-      // if (response.data.length > 0) {
-      //   setUsers(response.data);
-      // }
+        .then((data) => { setUsers(data) })
+      
     } catch (e) {
       console.error(e);
     }
   };
 
-  useEffect(() => {
-    getUsersApi();
-  }, []);
 
-  const navigate = useNavigate();
+  const Example = () => {
+    const { isLoading, error, data } = useQuery('repoData', () =>
+      fetch('https://api.github.com/repos/tannerlinsley/react-query')
+        .then(res => res.json()
+      )
+    )
+
+    if ( isLoading ) return <p>Loading..</p>
+
+    if ( error ) return <p>'error: ' + error.message</p>
+
+    console.log("data: ", data)
+
+    return (
+      <div>
+        <h1>{data.name}</h1>
+        <br/>
+        <p>{data.description}</p>
+        <br/>
+        <strong>{data.subscribers_count}</strong>
+        <br/>
+        <strong>{data.stargazers_count}</strong>
+        <br/>
+        <strong>{data.forks_count}</strong>
+      </div>
+    )
+  }
+
+
+  
   return (
     <S.Container>
       <div>하이하이</div>
       <ListMap list={users} component={<List />} />
+      <br/>
+      <br/>
+      <hr/>
+      <QueryClientProvider client={queryClient}>
+        <Example />
+      </QueryClientProvider>
     </S.Container>
   );
 };
